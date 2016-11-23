2f33e9e44945da0e847193d5aac7a88db054d67e
David Bradway <dpb6@duke.edu>
2016-11-22 19:43:35 -0500

    Add new file

---
+++ Database.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sqlite3
+
+# Open Connection to SQLite database
+con = sqlite3.connect("../documents.sqlite")
+
+# Read CSV into a pandas DataFrame
+df = pd.read_csv('../scholars_publications.csv', sep="\t")
+print(df.head())
+df.to_sql('table_name', con, if_exists='replace', index=False)
+
+# verify that result of SQL query stores the dataframe
+df2 = pd.read_sql_query("SELECT * from table_name", con)
+print(df2.head())
+
+con.close()
