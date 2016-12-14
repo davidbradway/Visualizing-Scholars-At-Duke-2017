980f1969a464ba2118717188eab1dcd132f57948
David Bradway <dpb6@duke.edu>
2016-12-14 15:10:22 -0500

    add code for bacon number and suggest coauthors

--- Python-Pandas_CSV.py
+++ Python-Pandas_CSV.py
@@ -1,18 +1,55 @@
 import pandas as pd
-import numpy as np
 import scipy.io as sio
+# import numpy as np
 
-author = pd.read_csv('../author.txt', encoding='cp1252',sep=";")
-print(author.head())
+author = pd.read_csv('../author.txt', encoding='cp1252', sep=";")
+# print(author.head())
 
-faculty= pd.read_csv('../scholars_faculty.csv', sep="\t")
-print(faculty.head())
+faculty = pd.read_csv('../scholars_faculty.csv', sep="\t")
+# print(faculty.head())
 
 mat_contents = sio.loadmat('../coauthornet.mat')
 coauthornet = mat_contents['coauthornet']
 # Numpy NdArray
 
-coauthor = pd.DataFrame(coauthornet,    # values
-                        index=author['author name'],    # Names as index
-                        columns=author['author name'])  # Names as column names
+coauthornetDF = pd.DataFrame(coauthornet,    # values
+                             index=author['author name'],    # Names as index
+                             columns=author['author name'])  # Names as column names
+# print(coauthor.head(2))
 
+"""
+With whom should I be collaborating? Suggest Facebook friends
+Select all of my coauthors by name or key. (each non zero entry in my row)
+Select all their coauthors
+filter out those who were my coauthors
+Rank the remaining coauthors by the number of papers my coauthors published with them
+"""
+# coauthor['Gregg E. Trahey']['Kathryn Radabaugh Nightingale']
+
+
+def get_coauthors(name):
+    coauthors = coauthornetDF.loc[coauthornetDF[name] > 0, [name]]
+    return coauthors
+
+
+def get_cocoauthors(name):
+    mycoauthors = get_coauthors(name)
+
+    # Create an empty DataFrame for the results
+    mycocoauthors = pd.DataFrame()
+
+    # For each coauthor, get *their* coauthors
+    for mycoauthor, row in mycoauthors.iterrows():
+        # print(index)
+        theircoauthors = get_coauthors(mycoauthor)
+        mycocoauthors = pd.concat([mycocoauthors, theircoauthors], axis=1)
+
+    return mycocoauthors, mycoauthors
+
+
+if __name__ == "__main__":
+    mycocoauthors, mycoauthors = get_cocoauthors('Gregg E. Trahey')
+
+
+
+    print('end')
--- bfs.py
+++ bfs.py
@@ -4,75 +4,122 @@ import pandas as pd
 import scipy.io as sio
 import pickle
 import os
+import random
 
+author = pd.read_csv('../author.txt', encoding='cp1252', sep=";")
+# print(author.head())
 
-def bfs_paths(graph, start, goal):
-    queue = [(start, [start])]
-    while queue:
-        (vertex, path) = queue.pop(0)  # pop it from the bottom of the stack, not the last-in
-        for nextNode in graph[vertex] - set(path):
-            if nextNode == goal:
-                yield path + [nextNode]
-            else:
-                queue.append((nextNode, path + [nextNode]))
-
-
-def shortest_bfs_path(graph, start, goal):
-    try:
-        return next(bfs_paths(graph, start, goal))
-    except StopIteration:
-        return None
-
-
-if __name__ == '__main__':
-    """
-    mygraph = {'A': {'B', 'C'},
-           'B': {'A', 'D', 'E'},
-           'C': {'A', 'F'},
-           'D': {'B'},
-           'E': {'B', 'F'},
-           'F': {'C', 'E'}}
-    found_path = shortest_bfs_path(mygraph, 'A', 'F')  # ['A', 'C', 'F']
-    print(found_path)
-    """
-
-    author = pd.read_csv('../author.txt', encoding='cp1252', sep=";")
-    # print(author.head())
-
+# Load from file if possible
+fname = '../coauthorgraph.pkl'
+if os.path.isfile(fname):
+    pkl_file = open(fname, 'rb')
+    coauthorgraph = pickle.load(pkl_file)
+    pkl_file.close()
+else:
     mat_contents = sio.loadmat('../coauthornet.mat')
     coauthornet = mat_contents['coauthornet']
     # NdArray
 
-    coauthor = pd.DataFrame(coauthornet,    # values
-                            index=author['row index'],    # Names as index
-                            columns=author['row index'])  # Names as column names
+    coauthor = pd.DataFrame(coauthornet,  # values
+                            index=author['row index'],
+                            columns=author['row index'])
     # print(coauthor.head())
 
-    # Load from file if possible
-    fname = '../coauthorgraph.pkl'
-    if os.path.isfile(fname):
-        pkl_file = open(fname, 'rb')
-        coauthorgraph = pickle.load(pkl_file)
-        pkl_file.close()
-    else:
-        # Adjacency list (for sparse graphs)
-        coauthorgraph = {}
-        for index, row in coauthor.iterrows():
-            myset = set()
-            for colindex, col in row.iteritems():
-                if col > 0 and index != colindex:
-                    # print(index, colindex, col)
-                    myset |= {colindex}
-            if myset:
-                coauthorgraph[index] = myset
+    # Adjacency list (for sparse graphs)
+    coauthorgraph = {}
+    for index, row in coauthor.iterrows():
+        myset = set()
+        for colindex, col in row.iteritems():
+            if col > 0 and index != colindex:
+                # print(index, colindex, col)
+                myset |= {colindex}
+        if myset:
+            coauthorgraph[index] = myset
 
-        pkl_file = open(fname, 'wb')
-        pickle.dump(coauthorgraph, pkl_file)
-        pkl_file.close()
+    pkl_file = open(fname, 'wb')
+    pickle.dump(coauthorgraph, pkl_file)
+    pkl_file.close()
 
-    found_path = shortest_bfs_path(coauthorgraph, 11, 286)
-    # print(found_path)
 
+def bfs_paths(graph, start, goal):
+    # The queue contains a list of current nodes and the path to each of those nodes
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)  # pop it from the bottom of the stack, not the last-in
+        # check all nodes before going to next depth
+        # do not recheck any nodes already visited in the path
+        for nextNode in graph[vertex] - set(path):
+            if nextNode == goal:
+                # Found the goal! Return the path with the nextNode (goal) at the end
+                yield path + [nextNode]
+            else:
+                # Did not find the goal, add the nextNode to the queue for visiting later
+                queue.append((nextNode, path + [nextNode]))
+
+
+def shortest_bfs_path(graph, start, goal):
+    # check for trivial case
+    if start == goal:
+        return [start]
+
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return []
+
+
+def test_graph():
+    mygraph = {'A': {'B', 'C'},
+               'B': {'A', 'D', 'E'},
+               'C': {'A', 'F'},
+               'D': {'B'},
+               'E': {'B', 'F'},
+               'F': {'C', 'E'}}
+    found_path1 = shortest_bfs_path(mygraph, 'A', 'F')
+    print(found_path1)
+    # ['A', 'C', 'F']
+
+
+def find_bacon(faculty1 = 11, faculty2 = 286):
+    try:
+        found_path = shortest_bfs_path(coauthorgraph, faculty1, faculty2)
+        # print(found_path)
+        return found_path
+    except KeyError:
+        found_path = []
+        return found_path
+
+
+def print_path(found_path):
     if found_path:
         for link in found_path:
-            print(author.loc[link - 1]['author name'])
+            print(link, author.loc[link - 1]['author name'])
+
+
+def loop_across(threshold = 8):
+    longest_short_path = []
+
+    lis1 = list(range(len(author)))
+    random.shuffle(lis1)
+    lis2 = list(range(len(author)))
+    random.shuffle(lis2)
+
+    for faculty1 in lis1:
+        print(author.loc[faculty1]['author name'])
+
+        for faculty2 in lis2:
+            current_path = find_bacon(faculty1 + 1, faculty2 + 1)
+
+            if len(current_path) > 1:
+                if len(current_path) > len(longest_short_path):
+                    longest_short_path = current_path
+                    print_path(longest_short_path)
+                    print(" ")
+            if len(longest_short_path) > threshold:
+                return longest_short_path
+
+if __name__ == '__main__':
+    # longest_short_path = find_bacon(11, 284)
+    # print_path(longest_short_path)
+
+    path = loop_across(8)
