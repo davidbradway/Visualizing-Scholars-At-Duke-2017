12844db56da3a2e90d306a287ccba07193ccafb4
David Bradway <dpb6@duke.edu>
2016-12-29 11:40:44 -0500

    run server on port 80 - usually not recommended

--- top5/application.py
+++ top5/application.py
@@ -105,4 +105,5 @@ def index():
     return render_template("search.html", form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=False)
+    app.run(host='0.0.0.0', port=80)
